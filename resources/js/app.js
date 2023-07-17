3beff4816489008754e64e18649587c6fc22b212
Vlad <vladek2000@inbox.ru>
2023-07-17 19:50:35 +0400

    add npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+import { createApp } from 'vue';
+import Index from "./components/Index.vue";
+
+const app = createApp({});
+
+app.component('index', Index)
+
+app.mount('#app')
